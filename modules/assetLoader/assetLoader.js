5a386dbbfd162caf2b8aa9324cf3086866037a52
noriyuki1985 <60012829+noriyuki1985@users.noreply.github.com>
2025-04-27 01:58:43 +0900

    Update assetLoader.js

--- modules/assetLoader/assetLoader.js
+++ modules/assetLoader/assetLoader.js
@@ -1,19 +1,27 @@
 // modules/assetLoader/assetLoader.js
+import CONFIG from '../js/config.js'; // main.js と同じ場所にある場合は相対パスを調整
+
 const assetLoader = {
   images: {},
+
   loadImages() {
     const keys = [
+      // タイル
       'tile_grass','tile_road',
+      // 敵
       ...CONFIG.ENEMY_DEFINITIONS.map(e => e.spriteKey),
+      // タワー
       ...CONFIG.TOWER_DEFINITIONS.map(t => t.spriteKey),
+      // 発射体
       ...CONFIG.PROJECTILE_DEFINITIONS.map(p => p.spriteKey)
     ];
+
     return Promise.all(
       keys.map(key => {
-        const category = key.startsWith('tile_')  ? 'tiles'
-                        : key.startsWith('enemy_') ? 'enemies'
-                        : key.startsWith('tower_') ? 'towers'
-                        : 'projectiles';
+        const category = key.startsWith('tile_')   ? 'tiles'
+                        : key.startsWith('enemy_')  ? 'enemies'
+                        : key.startsWith('tower_')  ? 'towers'
+                        :                             'projectiles';
         const img = new Image();
         img.src = `${CONFIG.ASSETS_PATH}/${category}/${key}.png`;
         this.images[key] = img;
@@ -21,8 +29,10 @@ const assetLoader = {
       })
     );
   },
+
   getImage(key) {
     return this.images[key];
   }
 };
+
 export default assetLoader;

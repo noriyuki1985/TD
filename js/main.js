070f03b586ae760892092a1d8d76cb04db8c7666
noriyuki1985 <60012829+noriyuki1985@users.noreply.github.com>
2025-04-27 03:07:35 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 // js/main.js
 // ─────────────────────────────────
-// ESモジュールとして読み込む前提です
+// ESモジュールとして読み込まれます
 
 import assetLoader    from '../modules/assetLoader/assetLoader.js';
-import audioManager   from '../modules/audio/audioManager.js';
 import stageLoader    from '../modules/stageLoader/stageLoader.js';
 import renderer       from '../modules/renderer/renderer.js';
 import playModel      from '../scenes/play/playModel.js';
@@ -12,9 +11,8 @@ import playController from '../scenes/play/playController.js';
 
 Promise.all([
   assetLoader.loadImages(),
-  audioManager.init(),
   stageLoader.loadStage(CONFIG.STAGE_JSON)
-]).then(([, , stageData]) => {
+]).then(([, stageData]) => {
   renderer.init('game-canvas', assetLoader.images);
   playModel.init(stageData);
   playView.init(renderer, playModel);

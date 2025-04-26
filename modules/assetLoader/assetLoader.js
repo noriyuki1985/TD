149cabb3d5f640a91bcaf234c949917f18a0ef9a
noriyuki1985 <60012829+noriyuki1985@users.noreply.github.com>
2025-04-27 01:33:43 +0900

    Update assetLoader.js

--- modules/assetLoader/assetLoader.js
+++ modules/assetLoader/assetLoader.js
@@ -1,29 +1,25 @@
+// modules/assetLoader/assetLoader.js
 const assetLoader = {
   images: {},
-  audios: {},
   loadImages() {
     const keys = [
-      // タイル
-      'tile_grass','tile_road','tile_water','tile_rock',
-      // 敵
+      'tile_grass','tile_road',
       ...CONFIG.ENEMY_DEFINITIONS.map(e => e.spriteKey),
-      // タワー
       ...CONFIG.TOWER_DEFINITIONS.map(t => t.spriteKey),
-      // 発射体
       ...CONFIG.PROJECTILE_DEFINITIONS.map(p => p.spriteKey)
     ];
-    const promises = keys.map(key => {
-      const img = new Image();
-      let category;
-      if (key.startsWith('tile_')) category = 'tiles';
-      else if (key.startsWith('enemy_')) category = 'enemies';
-      else if (key.startsWith('tower_')) category = 'towers';
-      else category = 'projectiles';
-      img.src = `${CONFIG.ASSETS_PATH}/${category}/${key}.png`;
-      this.images[key] = img;
-      return new Promise(res => img.onload = res);
-    });
-    return Promise.all(promises);
+    return Promise.all(
+      keys.map(key => {
+        const category = key.startsWith('tile_')  ? 'tiles'
+                        : key.startsWith('enemy_') ? 'enemies'
+                        : key.startsWith('tower_') ? 'towers'
+                        : 'projectiles';
+        const img = new Image();
+        img.src = `${CONFIG.ASSETS_PATH}/${category}/${key}.png`;
+        this.images[key] = img;
+        return new Promise(res => img.onload = res);
+      })
+    );
   },
   getImage(key) {
     return this.images[key];
